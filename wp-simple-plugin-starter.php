6ad887b5e703f2d1110e091342cfb68f329e68c0
Yassine <yassine.abouzia@gmail.com>
2023-12-21 16:28:00 +0100

    added plugin info

---
+++ wp-simple-plugin-starter.php
@@ -0,0 +1,9 @@
+/**
+* Plugin Name: WP Simple Plugin Starter
+* Description: A simple plugin starter.
+* Version: 1.0.0
+* Author: Yassine Abouzia
+* License: GPL v2 or later
+* License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
