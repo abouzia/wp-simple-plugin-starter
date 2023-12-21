9ea1833ca4d41069f977b52fda913ad236339127
Yassine <yassine.abouzia@gmail.com>
2023-12-21 16:33:30 +0100

    added plugin activation, desactivation and unistallation hooks

---
+++ uninstall.php
@@ -0,0 +1,5 @@
+<?php
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    exit();
+}
+// The code to run when the user clicks on the uninstall button.
--- wp-simple-plugin-starter.php
+++ wp-simple-plugin-starter.php
@@ -1,3 +1,4 @@
+<?php
 /**
 * Plugin Name: WP Simple Plugin Starter
 * Description: A simple plugin starter.
@@ -7,3 +8,16 @@
 * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
 */
 
+/** Upon activation */
+register_activation_hook( __FILE__, 'my_plugin_activation' );
+
+function my_plugin_activation() {
+    
+}
+
+/** Upon deactivation */
+register_deactivation_hook( __FILE__, 'my_plugin_deactivation' );
+
+function my_plugin_deactivation() {
+    
+}
